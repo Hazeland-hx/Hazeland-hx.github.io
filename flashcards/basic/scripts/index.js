1264515c052741e69b974f7b1d1adeccec0f453f
Hazeland-hx <82297981+Hazeland-hx@users.noreply.github.com>
2021-06-29 02:39:08 +0800

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,5 +1,7 @@
 
 var body = document.querySelector("body");
+var log = document.querySelector("#log");
+
 
 class question {
   // A constructor is a special function used to create an instance of the class
@@ -13,7 +15,7 @@ class question {
 
 // Declare a new array
 let myFlashcards = [];
- 
+
 // Create three questions and push them onto the myFlashcards array
 myFlashcards.push(new question('What is 3+5?', '8'));
 myFlashcards.push(new question('What color is the sky?', 'blue'));
@@ -22,7 +24,7 @@ myFlashcards.push(new question('Is ice cold?', 'yes'));
 // Loop through the array printing out the question and the answer
 let i = 0;
 while (i < myFlashcards.length)  {
-    console.log(myFlashcards[i].question + ' ' + myFlashcards[i].answer);
+    log.innerText =console.log(myFlashcards[i].question + ' ' + myFlashcards[i].answer);
     i=i+1;
 }
 
